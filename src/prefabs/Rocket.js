ef61736204382987b40390d359426b39c9c35a62
qwang122 <1157615907@qq.com>
2024-01-24 17:42:29 -0800

    bug fixed

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,24 +7,27 @@ class Rocket extends Phaser.GameObjects.Sprite{
         //add object to existing scene 
         scene.add.existing(this);
         /*** ask whats this is here 
+         * 
          * */ 
         //because "this" is already have the object 
         this.isFiring = false;
         //this.p1Rocket.x = this.input.mousePointer.x;
         this.MoveSpeed = 2;
+        this.sfxShot = scene.sound.add('sfx_shot')
 
     }
     update(){
         if(!this.isFiring){
-            if(this.moveLeft.isDown && this.x >= borderUISize + this.width){
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
                 this.x -=this.MoveSpeed; // if not outside of the border nad if keyLeft is pressed down decrement the x coordinate
-            }else if (this.moveRight.isDown && this.x <= game.config.width - borderUISize - this.width){
+            }else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
                 this.x += this.MoveSpeed;// if not outside of the border nad if keyRight is pressed down inecrement the x coordinate
             } 
         }
-        if(Phaser.Input.Keyboard.JustDown(this.Fire)){   // justdown is different than isdown just down wont take any other input?
+        if(Phaser.Input.Keyboard.JustDown(keyFIRE)){   // justdown is different than isdown just down wont take any other input?
             this.isFiring = true ;
-            this.sfxRocket.play();
+            this.sfxShot.play()
+            
         }
         
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
@@ -39,5 +42,5 @@ class Rocket extends Phaser.GameObjects.Sprite{
         this.isFiring = false;
         this.y = game.config.height - borderUISize - borderPadding;
     }
-    
+
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,25 +1,75 @@
-/********
- * init() prepares any data for the scene
- * preload() prepares any assets we’ll need for the scene
- * create() adds objects to the scene
- * update() is a loop that runs continuously (every frame, ideally) and allows us to update logic and game objects
- */
-class Menu extends Phaser.Scene{
-    constructor(){
-        super("menuScene")
-    }
-
-preload() {
-  // load images/tile sprites
-  this.load.image('rocket', './assets/rocket.png')
-  this.load.image('spaceship', './assets/spaceship.png')
-  this.load.image('starfield', './assets/starfield.png')
-}
-create() {  
-    this.add.text(20, 20, "Rocket Patrol Menu")
-    // skiping menu scene
-    this.scene.start("playScene")
+class Menu extends Phaser.Scene {
+  constructor() {
+      super ("menuScene");
+      
   }
-
-
+  preload() {
+      // load audio
+      this.load.audio('sfx_select', './assets/sfx-select.wav');
+      this.load.audio('sfx_explosion', './assets/sfx-explosion.wav');
+      this.load.audio('sfx_shot', './assets/sfx-shot.wav');
+      this.load.image('rocket', './assets/rocket.png')
+      this.load.image('spaceship', './assets/spaceship.png')
+      this.load.image('starfield', './assets/starfield.png')
+      this.load.spritesheet('explosion', './assets/explosion.png', {
+        frameWidth: 64,
+        frameHeight: 32,
+        startFrame: 0,
+        endFrame: 9
+    })
+    }
+  create(){
+      let menuConfig = {
+          fontFamily: 'Courier',
+          fontSize: '28px',
+          backgroundColor: '#F3B141',
+          color: '#843605',
+          align: 'right',
+          padding: {
+          top: 5,
+          bottom: 5,
+          },
+          fixedWidth: 0
+      }
+      
+      this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL',menuConfig).setOrigin(0.5);
+      //this.scene.start(“playScene”)
+      this.add.text(game.config.width/2, game.config.height/2, 'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+      menuConfig.backgroundColor = "#00FF00";
+      menuConfig.color = '#000';
+      this.add.text(game.config.width/2, game.config.height/2 + borderUISize +borderPadding, 'Press <- for Novice or -> for Expert',menuConfig).setOrigin(0.5);
+      //this.scene.start("playScene");
+      // define keys
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+      // animation configuration
+      this.anims.create({
+        key: 'explode',
+        frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
+        frameRate: 30
+    })
+  }
+  update() {
+      if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+        // easy mode
+        game.settings = {
+          spaceshipSpeed: 3,
+          SpacebugSpeed : 5,
+          gameTimer: 60000  
+        }
+        this.sound.play('sfx_select');
+        this.scene.start('playScene');    
+      }
+      if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+        // hard mode
+        game.settings = {
+          spaceshipSpeed: 4,
+          SpacebugSpeed : 6,
+          gameTimer: 45000    
+        }
+        this.sound.play('sfx_select');
+        this.scene.start('playScene');    
+      }
+    }
+  
 }
